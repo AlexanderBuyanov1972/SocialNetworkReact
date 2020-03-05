01398e17161b16c0bfcb4b812d2ba5d91ff00406
AlexanderBuyanov <globalist72@gmail.com>
2020-03-05 23:28:21 +0200

    2020-03-05

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import styles from './NavBar.module.css';
+import {NavLink} from 'react-router-dom';
 
 let NavBar = () => {
     return (
         <nav className={styles.nav}>
-            <div className={styles.item}>
-                <a href='/content'>Content</a>
-            </div>
-            <div className={styles.item}>
-                <a href='/dialogs'>Dialogs</a>
+            <div>
+                <NavLink className={styles.item} activeClassName={styles.active} to='/content'>Content</NavLink>
             </div>
             <div>
-                <a href='/news'>News</a>
+                <NavLink className={styles.item} activeClassName={styles.active} to='/dialogs'>Dialogs</NavLink>
             </div>
             <div>
-                <a href='/musics'>Musics</a>
+                <NavLink className={styles.item} activeClassName={styles.active} to='/news'>News</NavLink>
             </div>
             <div>
-                <a href='/settings'>Settings</a>
+                <NavLink className={styles.item} activeClassName={styles.active} to='/musics'>Musics</NavLink>
+            </div>
+            <div>
+                <NavLink className={styles.item} activeClassName={styles.active} to='/settings'>Settings</NavLink>
             </div>
         </nav>
     );
