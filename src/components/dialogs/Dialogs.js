f655cca2613517011060c648f06d1ef3de0cfb9c
AlexanderBuyanov <globalist72@gmail.com>
2020-03-06 00:47:59 +0200

    2020-03-06

--- src/components/dialogs/Dialogs.js
+++ src/components/dialogs/Dialogs.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import styles from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) => {
     return (
-        <div>
-        Dialogs
-         </div>
+
+        <div className={styles.dialogs}>
+            <div className={styles.dialogs_items}>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    <NavLink to='/dialogs/1' className={styles.item}>Dima</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/2' className={styles.item}>Sveta</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/3' className={styles.item}>Andrey</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/4' className={styles.item}>Sasha</NavLink>
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>
+                    Hi
+                </div>
+                <div className={styles.message}>
+                    How are you?
+                </div>
+                <div className={styles.message}>
+                    Yo!
+                </div>
+            </div>
+        </div>
+
+
     );
 }
 
