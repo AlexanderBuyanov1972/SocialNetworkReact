7d3fb5f1fbb4d8f52ba7653e107fffaa5fa95e9e
AlexanderBuyanov1972 <AlexanderBuyanov1972@users.noreply.github.com>
2020-05-14 22:31:56 +0300

    Updates

---
+++ precache-manifest.8604343877be9fa894dd4c6dc1e4275d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4250d7b8a44106fba3847e2edc45408",
+    "url": "/SocialNetworkReact/index.html"
+  },
+  {
+    "revision": "d17002797046d03f75d6",
+    "url": "/SocialNetworkReact/static/css/3.84f551ed.chunk.css"
+  },
+  {
+    "revision": "acb27b0ede8d8e9d99f7",
+    "url": "/SocialNetworkReact/static/css/4.b4064f4b.chunk.css"
+  },
+  {
+    "revision": "2e2704c4d4903bb249c3",
+    "url": "/SocialNetworkReact/static/css/main.e8855176.chunk.css"
+  },
+  {
+    "revision": "f498649445e32b6a4e3f",
+    "url": "/SocialNetworkReact/static/js/2.9736ee4a.chunk.js"
+  },
+  {
+    "revision": "a51e1a732616a65fe21698cc04856fb1",
+    "url": "/SocialNetworkReact/static/js/2.9736ee4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17002797046d03f75d6",
+    "url": "/SocialNetworkReact/static/js/3.7edd80e6.chunk.js"
+  },
+  {
+    "revision": "acb27b0ede8d8e9d99f7",
+    "url": "/SocialNetworkReact/static/js/4.6799e47d.chunk.js"
+  },
+  {
+    "revision": "2e2704c4d4903bb249c3",
+    "url": "/SocialNetworkReact/static/js/main.2a085829.chunk.js"
+  },
+  {
+    "revision": "8c5630bf6068f6c73d7d",
+    "url": "/SocialNetworkReact/static/js/runtime-main.b87d6c81.js"
+  },
+  {
+    "revision": "edc625f7bb114ac647c21b6b8c80b7be",
+    "url": "/SocialNetworkReact/static/media/preloader.edc625f7.svg"
+  }
+]);
